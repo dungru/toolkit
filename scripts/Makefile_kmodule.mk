3f338c4b5871ef545f7aa5767e3da00e20b922e0
Dung-Ru Tsai <octobersky.tw@gmail.com>
2020-11-15 10:52:57 +0800

    Create Makefile_kmodule.mk

---
+++ scripts/Makefile_kmodule.mk
@@ -0,0 +1,48 @@
+CONFIG_MODULE_SIG=n
+TARGET_MODULE := mazu_emu
+obj-m := $(TARGET_MODULE).o
+
+$(TARGET_MODULE)-objs := mazu_emu_main.o
+
+
+ccflags-y := -std=gnu99 -Wno-declaration-after-statement
+
+KDIR := /lib/modules/$(shell uname -r)/build
+PWD := $(shell pwd)
+
+GIT_HOOKS := ../.git/hooks/applied
+
+# all: $(GIT_HOOKS)
+all:
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+
+# $(GIT_HOOKS):
+# 	@scripts/install-git-hooks
+# 	@echo
+
+clean:
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
+#	$(RM) client out
+load:
+	sudo insmod $(TARGET_MODULE).ko
+unload:
+	sudo rmmod $(TARGET_MODULE) || true >/dev/null
+# all: $(GIT_HOOKS) client
+# client: client.c
+# 	$(CC) -o $@ $^
+
+PRINTF = env printf
+PASS_COLOR = \e[32;01m
+NO_COLOR = \e[0m
+pass = $(PRINTF) "$(PASS_COLOR)$1 Passed [-]$(NO_COLOR)\n"
+
+# check: all
+# 	$(MAKE) unload
+# 	$(MAKE) load
+# 	echo 2 > /proc/fibonacci/fib_flag 
+# 	sudo ./client > out
+# 	$(MAKE) unload
+# 	@diff -u out scripts/expected.txt && $(call pass)
+# 	@scripts/verify.py
+
+.PHONY : clean
